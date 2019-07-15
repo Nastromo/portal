cc6bb70b50b33c2d7afee8a3d2716054132c62cf
nastromo <nastromo@Solaris.local>
2019-07-15 21:04:34 +0300

    deploy fixes

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cp -R middleware prod
-cp -R routes prod
 cp -R models prod
+cp -R routes prod
 cp -R utils prod
 cp app.js prod
 cp db.js prod
@@ -11,14 +11,14 @@ cp package.json prod
 cd prod
 tar czf app.tar.gz middleware/ models/ routes/ utils/ app.js db.js package.json .env
 
-sftp -i "devempr.pem" ubuntu@192.168.20.204 << 'ENDSSH'
+sftp -i "empraws.pem" ec2-user@18.222.204.34 << 'ENDSSH'
 put app.tar.gz
 exit
 ENDSSH
 
 rm app.tar.gz
 
-ssh -i "devempr.pem" ubuntu@192.168.20.204 << 'ENDSSH'
+ssh -i "empraws.pem" ec2-user@18.222.204.34 << 'ENDSSH'
 pm2 stop portal
 rm -rf portal_back/* portal_back/.*
 tar xf app.tar.gz -C portal_back
